9e46eee2f26fa5f77656b07b99c0f33297b0bf15
currenjin <hyun0524e@naver.com>
2024-07-02 19:05:44 +0900

    Init: Gradle Project

---
+++ src/main/java/com/currenjin/easy_jpa/EasyJpaApplication.java
@@ -0,0 +1,13 @@
+package com.currenjin.easy_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/currenjin/easy_jpa/EasyJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currenjin.easy_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
