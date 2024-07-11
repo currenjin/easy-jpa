f592f0435ae3aa0fbbd04faa2803fe4502319107
currenjin <hyun0524e@naver.com>
2024-07-11 12:58:18 +0900

    Feat: Use a new repository

--- src/main/java/com/currenjin/easy_jpa/EasyJpaApplication.java
+++ src/main/java/com/currenjin/easy_jpa/EasyJpaApplication.java
@@ -1,13 +1,11 @@
 package com.currenjin.easy_jpa;
 
 import com.currenjin.easy_jpa.domain.Book;
+import com.currenjin.easy_jpa.service.BookService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 
 @SpringBootApplication
 public class EasyJpaApplication {
@@ -17,23 +15,13 @@ public class EasyJpaApplication {
 	}
 
 	@Bean
-	public CommandLineRunner example(EntityManagerFactory entityManagerFactory) {
+	public CommandLineRunner demo(BookService bookService) {
 		return (args) -> {
-			EntityManager entityManager = entityManagerFactory.createEntityManager();
+			Book book = new Book("JPA", "currenjin");
+			bookService.saveBook(book);
 
-			entityManager.getTransaction().begin();
-
-			// 엔티티 생성 및 저장 예제
-			Book book = new Book("Test Driven Development", "Currenjin");
-			entityManager.persist(book);
-
-			entityManager.getTransaction().commit();
-
-			// 엔티티 조회 예제
-			Book foundBook = entityManager.find(Book.class, book.getId());
+			Book foundBook = bookService.findBookById(book.getId());
 			System.out.println("Found Book: " + foundBook.getTitle() + " by " + foundBook.getAuthor());
-
-			entityManager.close();
 		};
 	}
 }
